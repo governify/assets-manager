698fa87af75551cc265e79a77800a0318ec3d58f
Alejandro Santisteban <72552837+alesancor1@users.noreply.github.com>
2021-09-21 18:04:27 +0200

    fix: lint fix

--- git-downloader.js
+++ git-downloader.js
@@ -19,11 +19,11 @@ function gitDownload (url, path, branch) {
     var process = spawnSync('git', args);
     if (process.stderr) {
       logger.info(`GIT-CLONE-CONSOLE: ${process.stderr.toString()}`);
-      resolve('Failed to clone')
+      resolve('Failed to clone');
     }
-    
+
     if (!empty) {
-      logger.info(`GIT-CLONE-CONSOLE: Directory not empty, attempting to merge with current files`);
+      logger.info("GIT-CLONE-CONSOLE: Directory not empty, attempting to merge with current files");
 
       fs.renameSync(`${path}/tmp/.git`, `${path}/.git`); // Moves .git folder
       fs.rmdirSync(`${path}/tmp`, { recursive: true });
@@ -31,10 +31,10 @@ function gitDownload (url, path, branch) {
       var reset = spawnSync('git', ['reset', '--hard', 'HEAD'], { cwd: path });
       if (reset.stderr) {
         logger.info(`GIT-CLONE-CONSOLE: ${process.stderr.toString()}`);
-        resolve('Failed to reset')
+        resolve('Failed to reset');
       }
-    } 
-    resolve('Cloned succesfully'); 
+    }
+    resolve('Cloned succesfully');
   });
 }
 
